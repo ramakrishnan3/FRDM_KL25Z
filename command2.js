ccd549658f510bb7ff5127560c752dbb4e1baf03
Ramakrishnan S <ramakrishnan.sathyanarayanan@solitontech.com>
2018-07-17 17:22:35 +0530

    Updated | DataCount, Time & Replacing '7e'
    Signed-off-by: Ramakrishnan S <ramakrishnan.sathyanarayanan@solitontech.com>

--- command2.js
+++ command2.js
@@ -1,25 +1,14 @@
 var SerialPort = require("serialport");
-// const MockBinding = SerialPort.Binding;
-// MockBinding.createPort('COM13');
 var port = new SerialPort("COM13", {
   baudRate: 115200,
   autoOpen: false
 });
 var fs = require('fs');
 var filePath = 'result.txt';
-// fs.writeFileSync(filePath,'Hi Hello');
 var readCmd = [126,36,1,0,3,224,13,1,126];
 var streamStart = [126,33,1,0,2,1,3,126];
 var streamStop = [126,33,1,0,2,2,1,126];
 var writeCmd = [126,35,1,0,3,224,42,25,126];
-
-function bin2String(array) {
-  var result = "";
-  for (var i = 0; i < array.length; i++) {
-    result += String.fromCharCode(array[i]);
-  }
-  return result;
-}
 
 function pack(bytes) {
   var str = "";
@@ -48,41 +37,38 @@ port.open(function (err){
   }
   
   port.write(pack(writeCmd));
-  // console.log('Written : ', port.write(pack(writeCmd)));
 });
 
 port.on('open', function() {
   console.log("Opened the port!")
 });
 
-// Switches the port into "flowing mode"
-// port.on('data', function (data) {
-  // console.log('Data:', data);
-  // var buffer = port.read();
-  // console.log('LENGTH : ', data.length, port.baudRate);
-// });
-
 // Read data that is available but keep the stream from entering "flowing mode"
 var data = [];
+var dataCount = 0;
+var startTime = new Date();
 port.on('readable', function () {
   setTimeout(function() {
     var buffer = port.read();
-    console.log(buffer);
     data = [];
+    dataCount = 0;
+    startTime = new Date();
     for (var i=0; i < buffer.length; i++) {
       if (buffer[i].toString(16) === '7e') {
         if (isStartBit(buffer, i)) {
             // If it is a start bit
             var startbit = i+2;
             var stopbit = findStopbit(buffer, i+1);
-            if (startbit && stopbit) 
+            if (startbit && stopbit) {
               appendData(buffer, startbit, stopbit);
+              i = stopbit;
+            }
             else if (startbit && stopbit === undefined)
               appendData(buffer, startbit, buffer.length);
         }
       }
     }
-    fs.appendFileSync(filePath, data + '\n');
+    fs.appendFileSync(filePath, '\nDATA COUNT : ' + dataCount + '\nTime : ' + (new Date() - startTime)+ '\n' + data);
   }, 1000);
 });
 
@@ -94,12 +80,6 @@ function isStartBit(buffer, i) {
   }
   return false;
 }
-
-// function isStopBit(buffer, index) {
-//   if (buffer[i+1] && buffer[i+1].toString(16) === '7e')
-//     return true;
-//   return false;
-// }
 
 function findStopbit(buffer, i) {
   for(var j=i;j < buffer.length; j++) {
@@ -114,5 +94,12 @@ function appendData(buffer, startIndex, stopIndex) {
   for (let k = startIndex; k < stopIndex; k++) {
     str += buffer[k].toString(16) + ' ';
   }
+  if (str.includes('7d 5e')) {
+    str = str.replace('7d 5e', '7e');
+  }
+  if (str.includes('7d 5d')) {
+    str = str.replace('7d 5d', '7d');
+  }
   data.push(str);
+  dataCount++;
 }
