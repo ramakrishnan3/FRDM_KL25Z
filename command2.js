470c9424584425c6be8698b04b570eebf9b87389
Ramakrishnan <ramakrishnan.sathyanarayanan@solitontech.com>
2018-07-18 16:23:35 +0530

    removed isStartBit function, added condition to take the escape sequence as data when it is intended to be a data

--- command2.js
+++ command2.js
@@ -1,6 +1,6 @@
 var SerialPort = require("serialport");
 var port = new SerialPort("COM6", {
-  baudRate: 230400,
+  baudRate: 576000,
   autoOpen: false
 });
 var fs = require('fs');
@@ -42,23 +42,17 @@ port.on('open', function() {
 
 // Read data that is available but keep the stream from entering "flowing mode"
 var data = [];
-var eb = 0;
 var excessBits = [];
-var dataCount = 0;
 var startTime = new Date();
 port.on('readable', function () {
   setTimeout(function() {    
     // var ar = [0, 255,126, 126, 0, 80, 221, 126,126, 0, 81, 221, 126,126, 0, 100, 216, 126, 126, 0, 93, 165, 126,126, 0, 92, 165, 126];
     // var buffer = Buffer.from(ar);
     var buffer = Buffer.concat([ Buffer.from(excessBits), port.read() ]);
+    excessBits = [];
     data = [];
-    dataCount = 0;
-    startTime = new Date();
-    // var start = extractExcessBits(buffer);
     for (var i=0; i < buffer.length; i++) {
       if (buffer[i].toString(16) === '7e') {
-        // if (isStartBit(buffer, i)) {
-            // If it is a start bit
             var startbit = i+2;
             var stopbit = findStopbit(buffer, i+1);
             if ( startbit > 0 && stopbit > 0 && (stopbit - startbit > 1)) {
@@ -67,32 +61,11 @@ port.on('readable', function () {
             }
             else if (startbit && stopbit === undefined)
               appendDatatoExcessBits(buffer, startbit, buffer.length);
-        // }
       }
     }
     fs.appendFileSync(filePath, data + ',');
   }, 50);
 });
-
-/* function extractExcessBits(buffer) {
-  if (buffer[0].toString(16) !== '7e' || !isStartBit(buffer, 0)) {
-    for (var start = 0; start < buffer.length; start++) {
-      if (buffer[start+1] && buffer[start + 1].toString(16) === '7e') {
-        appendData(buffer, 0, start+1, 'append');
-        break;
-      }
-    }
-  }
-} */
-
-function isStartBit(buffer, i) {
-  if (buffer[i-1] && buffer[i-1].toString(16) === '7e')
-    return true;
-  else if (!buffer[i-1] && i === 0 && buffer[i].toString(16) === '7e' && buffer[i+1].toString(16) !== '7e') {
-    return true;
-  }
-  return false;
-}
 
 function findStopbit(buffer, i) {
   for(var j=i;j < buffer.length; j++) {
@@ -105,33 +78,29 @@ function findStopbit(buffer, i) {
 
 function appendData(buffer, startIndex, stopIndex) {
   var str = '';
-  var temp = '';
   for (let k = stopIndex-1; k >= startIndex; k--) {
 	if(buffer[k] < 16)
 		str += '0' + buffer[k].toString(16);
 	else
 		str += buffer[k].toString(16);
   }
-  temp = str;
   data.push(parseInt(replaceWithOriginal(str), 16));
   if(data[data.length - 1] - data[data.length - 2] !== 1){
 
   }
-  dataCount++;
 }
 
 function appendDatatoExcessBits(buffer, startIndex, stopIndex) {
   excessBits  = [];
   for (let k = startIndex - 2; k < stopIndex; k++)
     excessBits.push(buffer[k]);
-  console.log('excess bits', eb++);
 }
 
 function replaceWithOriginal(str) {
-  if (str.includes('5d7d')) {
+  if (str.includes('5d7d') && str.length > 4) {
     str = str.replace(/5d7d/g, '7d');
   }
-  if (str.includes('5e7d')) {
+  if (str.includes('5e7d')  && str.length > 4) {
     str = str.replace(/5e7d/g, '7e');
   }
   return str;
